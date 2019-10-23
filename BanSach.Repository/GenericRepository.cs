a247cdf37a11c91bc24f75c79302facfa83de1f5
thuypn52 <thuypn52@wru.vn>
2019-10-23 22:04:29 +0700

    no message

---
+++ BanSach.Repository/GenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using BanSach.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanSach.Repository
+{
+    public class GenericRepository<T> where T:class
+    {
+        QuanLyBanSachEntities1 db = new QuanLyBanSachEntities1();
+    }
+}
---
+++ BanSach.Service/GenericService.cs
@@ -0,0 +1,18 @@
+﻿using BanSach.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanSach.Service
+{
+    public class GenericService<T> where T : class
+    {
+        protected readonly IType_Repository type_Repository;
+        public GenericService()
+        {
+            type_Repository = new Type_Repository();
+        }
+    }
+}
